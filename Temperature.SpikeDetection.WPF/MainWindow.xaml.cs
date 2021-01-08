0aafe2e18191bc65bd7a47e20440fdba225b0c11
Alejandro <averagonzalez@omnitech-inc.com>
2021-01-08 11:32:13 -0600

    Finished UI

--- Temperature.SpikeDetection.WPF/MainWindow.xaml.cs
+++ Temperature.SpikeDetection.WPF/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
+using LiveCharts;
+using LiveCharts.Wpf;
+using Microsoft.ML;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using TemperatureEstimation;
+using TemperatureEstimation.DataStructure;
 
 namespace Temperature.SpikeDetection.WPF
 {
